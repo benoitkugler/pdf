b2a341243d5cf435095d4328e4fa3e05accf86f6
Benoit KUGLER <x.ben.x@free.fr>
2020-11-07 21:59:01 +0100

    cleanup

--- reader/cmaps/const.go
+++ reader/cmaps/const.go
@@ -1,8 +1,3 @@
-/*
- * This file is subject to the terms and conditions defined in
- * file 'LICENSE.md', which is part of this source code package.
- */
-
 package cmap
 
 import (
--- reader/cmaps/primitives.go
+++ reader/cmaps/primitives.go
@@ -1,8 +1,3 @@
-/*
- * This file is subject to the terms and conditions defined in
- * file 'LICENSE.md', which is part of this source code package.
- */
-
 package cmap
 
 import "github.com/benoitkugler/pdf/model"
@@ -10,26 +5,10 @@ import "github.com/benoitkugler/pdf/model"
 type cmapObject interface {
 }
 
-// type cmapName struct {
-// 	Name string
-// }
-
 type cmapOperand string
 
 // cmapHexString represents a PostScript hex string such as <FFFF>
 type cmapHexString []byte
-
-// type cmapFloat struct {
-// 	val float64
-// }
-
-// type cmapInt struct {
-// 	val int64
-// }
-
-// type cmapString struct {
-// 	String string
-// }
 
 type cmapArray = []cmapObject
 
